54c671ecf87a5b876c8c4404de58ac01ef9229b7
Diya Nathwani <diyanathwani007@gmail.com>
2026-07-10 12:06:50 +0530

    fix: change login error message for deleted users to 'User not found'

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -88,7 +88,7 @@ const loginUser = async (req, res) => {
     if (!user || user.isDeleted) {
       return res.status(400).json({
         success: false,
-        message: user?.isDeleted ? "This account has been deleted. Please contact support." : "Invalid Email",
+        message: user?.isDeleted ? "User not found" : "Invalid Email",
       });
     }
 
